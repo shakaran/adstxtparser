5518b64670ca1f22a9a4f926d3f630d6e566a805
Ángel Guzmán Maeso <shakaran@gmail.com>
2018-06-14 01:40:23 +0200

    <feature> Implement a test for external file. Test underministic

--- Tests/ParserTest.php
+++ Tests/ParserTest.php
@@ -15,11 +15,59 @@ class ParserTest extends TestCase
             $parser = new Parser();
             $parser->readExternalFile();
 
-            $this->assertFalse();
+            $this->assertFalse(TRUE);
         }
         catch(AdsFileNotFound $e)
         {
             $this->assertEquals($e->getMessage(), 'Error getting ads.txt file for the domain');
         }
+    }
+
+    public function testNewspaperExternalFile()
+    {
+        try
+        {
+            // By default localhost
+            $parser = new Parser();
+            $parser->readExternalFile('http://estaticos.elmundo.es');
+
+            // More examples to test:
+            // https://elpais.com/ads.txt
+            // https://www.elconfidencial.com/ads.txt
+
+            $this->assertTrue(TRUE);
+        }
+        catch(AdsFileNotFound $e)
+        {
+            $this->assertFalse(TRUE);
+        }
+
+        $comments = $parser->getComments();
+        // var_dump($comments);
+        $this->assertTrue(is_array($comments));
+
+        $errors = $parser->getErrors();
+        //var_dump($errors);
+        $this->assertTrue(is_array($errors));
+
+        $warnings = $parser->getWarnings();
+        // var_dump($warnings);
+        $this->assertTrue(is_array($warnings));
+
+        $fields = $parser->getFields();
+        // var_dump($fields);
+        $this->assertTrue(is_array($fields));
+
+        $variables = $parser->getVariables();
+        //var_dump($variables);
+        $this->assertTrue(is_array($variables));
+
+        $resellers = $parser->getResellers();
+        //var_dump(count($resellers));
+        $this->assertTrue(is_array($resellers));
+
+        $directs = $parser->getDirects();
+        //var_dump(count($directs));
+        $this->assertTrue(is_array($directs));
     }
 }
