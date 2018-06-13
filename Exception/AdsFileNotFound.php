db43adadc70696d4a6e462d5b992d5f427193ee1
Ángel Guzmán Maeso <shakaran@gmail.com>
2018-06-14 00:20:51 +0200

    <feature> Add AdsFileNotFound exception

---
+++ Exception/AdsFileNotFound.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AdsTxtParser\Exception;
+
+/**
+ * AdsTxtParser
+ *
+ * Compatible with Ads.txt Specification Version 1.0.1 (OpenRTB working group)
+ * https://iabtechlab.com/wp-content/uploads/2017/09/IABOpenRTB_Ads.txt_Public_Spec_V1-0-1.pdf
+ *
+ * @see https://iabtechlab.com/ads-txt/
+ * @author Ángel Guzmán Maeso <angel@guzmanmaeso.com>
+ *
+ * @license AGPLv3
+ */
+class AdsFileNotFound extends \ErrorException
+{
+
+}
